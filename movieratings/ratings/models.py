5d30042977c2e5472dddd8dbafb1290a32d46ab9
Jonathan Frederick <jrfreder@gmail.com>
2015-10-06 19:55:55 -0400

    added __str__ methods

--- movieratings/ratings/models.py
+++ movieratings/ratings/models.py
@@ -5,18 +5,22 @@ from django.db import models
 class Rater(models.Model):
     rater = models.IntegerField()
 
-
-
+    def __str__(self):
+        return str(self.rater)
 
 
 class Movie(models.Model):
     title = models.CharField(max_length=215)
     movie = models.IntegerField()
 
-
+    def __str__(self):
+        return str(self.movie) + ' - ' + self.title
 
 
 class Rating(models.Model):
     stars = models.SmallIntegerField()
     rater = models.ForeignKey(Rater)
     movie = models.ForeignKey(Movie)
+
+    def __str__(self):
+        return self.movie + ': ' + str(self.stars) + '(' + self.rater + ')'
