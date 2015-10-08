ef9722eaf749999fb83f371eb3c5807efd7fa752
Jonathan Frederick <jrfreder@gmail.com>
2015-10-08 14:04:06 -0400

    getting to show-rater.html

--- movieratings/ratings/urls.py
+++ movieratings/ratings/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^movies/top', views.top_20),
-    url(r'^movie/(?P<movie_id>\d+)', views.show_movie)
+    url(r'^movie/(?P<movie_id>\d+)', views.show_movie),
+    url(r'^rater/(?P<rater_id>\d+)', views.show_rater)
 ]
--- movieratings/ratings/views.py
+++ movieratings/ratings/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Movie
+from .models import Movie, Rater
 
 # Create your views here.
 
@@ -13,9 +13,17 @@ def top_20(request):
                   'ratings/top-20-movies.html',
                   {'top': movies})
 
+
 def show_movie(request, movie_id):
     movie = Movie.objects.get(pk=movie_id)
-    ratings = movie.rating_set.all()
+    # ratings = movie.rating_set.all()
     return render(request,
                   'ratings/show-movie.html',
                   {'movie': movie})
+
+
+def show_rater(request, rater_id):
+    rater = Rater.objects.get(pk=rater_id)
+    return render(request,
+                  'ratings/show-rater.html',
+                  {'rater': rater})
