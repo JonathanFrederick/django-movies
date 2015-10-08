0c8d6562c96eea07079abf3fe086a01477a8bcf0
Jonathan Frederick <jrfreder@gmail.com>
2015-10-08 13:42:14 -0400

    getting to basic movie page

--- movieratings/ratings/urls.py
+++ movieratings/ratings/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^movies/top', views.top_20)
+    url(r'^movies/top', views.top_20),
+    url(r'^movie/(?P<movie_id>\d+)', views.show_movie)
 ]
