fd34b215c9d6d026ecf8496566095e16d914f707
Jonathan Frederick <jrfreder@gmail.com>
2015-10-09 00:00:38 -0400

    progress on generating fake users

--- movieratings/ratings/models.py
+++ movieratings/ratings/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
-class Rater(models.Model):  # TODO Add demographic data functionality
+class Rater(models.Model):
     MALE = 'M'
     FEMALE = 'F'
     NOT_GIVEN = 'X'
@@ -17,6 +18,7 @@ class Rater(models.Model):  # TODO Add demographic data functionality
     zipcode = models.CharField(max_length=5, default='00000')
     age = models.PositiveIntegerField(default='0')
     occupation = models.CharField(max_length=40, default='00')
+    # profile = models.OneToOneField(User, null=True, primary_key=True)
 
     def __str__(self):
         return str(self.id)
@@ -44,11 +46,27 @@ class Rating(models.Model):
                                     str(self.rater))
 
 
+def create_fake_user(user_id, fake):
+    return {'fields': {'username': fake.user_name(),
+                       'email': fake.email(),
+                       'password':'password'},
+            'model': 'users.Profile',
+            'pk': user_id
+            }
 def load_initial_data():
     import csv
     import json
+    from faker import Faker
+    from users.models import Profile
+    # from django.contrib.auth.models import User
 
+    fake = Faker()
     raters = []
+    profiles = []
+    # list({User.objects
+    #                  .create_user(username=fake.user_name(),
+    #                               email=fake.email(),
+    #                               password='password') for _ in range(9001)})
     with open('data/users.dat') as f:
         reader = csv.DictReader([line.replace('::', '\t') for line in f],
                                 fieldnames='UserID::Gender::Age::Occupation::Zip-code'.split('::'),
@@ -60,13 +78,28 @@ def load_initial_data():
                     'age': row['Age'],
                     'occupation': row['Occupation'],
                     'zipcode': row['Zip-code'],
+                    # 'profile': profiles.pop(0)
+                    # User.objects
+                    #                .create_user(username=fake.user_name(),
+                    #                             email=fake.email(),
+                    #                             password='password')
                 },
                 'model': 'ratings.Rater',
                 'pk': int(row['UserID']),
             }
             raters.append(rater)
+            try:
+                new_profile = create_fake_user(int(row['UserID']), fake)
+            except:
+                print("HI", end=' ')
+                fake = Faker()
+                new_profile = create_fake_user(int(row['UserID']), fake)
+            # new_profile.save()
+            profiles.append(new_profile)
     with open('ratings/fixtures/raters.json', 'w') as f:
         f.write(json.dumps(raters))
+    with open('ratings/fixtures/profiles.json', 'w') as f:
+        f.write(json.dumps(profiles))
 
     movies = []
     with open('data/movies.dat', encoding='windows-1252') as f:
