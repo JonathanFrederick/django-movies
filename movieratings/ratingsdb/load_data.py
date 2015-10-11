6f2a80e0b4df88909a4c763d01a7641d70a9b02d
Jonathan Frederick <jrfreder@gmail.com>
2015-10-11 17:23:01 -0400

    making movies.json

--- movieratings/ratingsdb/load_data.py
+++ movieratings/ratingsdb/load_data.py
@@ -1,11 +1,24 @@
-import csv
+import csv, json
+
 
 def load_movies():
+    movies = []
     with open('data/movies.dat', encoding='windows-1252') as f:
         reader = csv.DictReader([line.replace('::', '\t') for line in f],
                                 fieldnames=['movie_id', 'title', 'genres'],
                                 delimiter='\t')
         for line in reader:
-            print(line)
+            movie = {
+                'fields': {
+                    'title': line['title']
+                },
+                'model': 'ratingsdb.Movie',
+                'pk': int(line['movie_id'])
+            }
+            movies.append(movie)
+
+    with open('fixtures/movies.json', 'w') as f:
+        f.write(json.dumps(movies))
+
 
 load_movies()
