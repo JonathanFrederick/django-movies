aef9c3bedfa6e705017e7f55157785e4245ab0d7
Jonathan Frederick <jrfreder@gmail.com>
2015-10-11 16:43:13 -0400

    tidying

--- movieratings/ratingsdb/tests.py
+++ movieratings/ratingsdb/tests.py
@@ -18,15 +18,7 @@ class MovieTestCase(TestCase):
                                                          user=test_user),
                               pk=1)
 
-    # test_mov = Movie.objects.create(title='TEST', pk=1)
-    # test_mov.save()
-    # print('HIHIHIHIHIHIHIHIH')
-
-        # test_mov = Movie()
-        # test_mov.title = 'TEST'
-        # test_mov.pk = 1
     def test_movie_creation(self):
-        # Movie.objects.create(title='TEST', pk=1)
         self.assertEquals(Movie.objects.get(pk=1).title, 'TEST')
         self.assertEquals(Movie.objects.get(title='TEST').pk, 1)
         self.assertNotEquals(Movie.objects.get(title='TEST').pk, 2)
@@ -37,12 +29,6 @@ class MovieTestCase(TestCase):
         self.assertEquals(Rating.objects.get(pk=1).rater.pk, 1)
 
     def test_rater_creation(self):
-        # test_user = User.objects.create_user(pk=1,
-        #                                      username='tester',
-        #                                      email='test@gmail.com',
-        #                                      password='password')
-        # Rater.objects.create(pk=1, gender='M', zipcode='55555',
-        #                      occupation=6, age=35, user=test_user)
         self.assertEqual(Rater.objects.get(pk=1).gender, 'M')
         self.assertEqual(Rater.objects.get(pk=1).zipcode, '55555')
         self.assertEqual(Rater.objects.get(pk=1).occupation, 6)
