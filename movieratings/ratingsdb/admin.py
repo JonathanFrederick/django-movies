8d2aeb902f71faa819f956a880575b2ad7122d76
Jonathan Frederick <jrfreder@gmail.com>
2015-10-11 16:54:53 -0400

    added new models

--- movieratings/ratingsdb/admin.py
+++ movieratings/ratingsdb/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Movie, Rater, Rating
 
 # Register your models here.
+
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+
+
+class RaterAdmin(admin.ModelAdmin):
+    list_display = ['pk']
+
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ['movie', 'rater', 'stars']
+
+
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Rater, RaterAdmin)
+admin.site.register(Rating, RatingAdmin)
