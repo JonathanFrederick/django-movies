a79e477c33b6e12a971624671a1539cc9984970e
Jonathan Frederick <jrfreder@gmail.com>
2015-10-11 17:02:22 -0400

    opening movie file

--- movieratings/ratingsdb/load_data.py
+++ movieratings/ratingsdb/load_data.py
@@ -1,1 +1,6 @@
-print('hello world')
+def load_movies():
+    with open('data/movies.dat', encoding='windows-1252') as f:
+        for line in f:
+            print(line)
+            
+load_movies()
