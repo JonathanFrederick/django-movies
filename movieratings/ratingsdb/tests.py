9b993107bf4cc461672310bf730d9b86bc228c78
Jonathan Frederick <jrfreder@gmail.com>
2015-10-11 15:50:57 -0400

    wrote tests for rater and rating creation

--- movieratings/ratingsdb/tests.py
+++ movieratings/ratingsdb/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Movie
+from .models import Movie, Rating, Rater
 # Create your tests here.
 
 
@@ -17,3 +17,22 @@ class MovieTestCase(TestCase):
         test_mov = Movie.objects.create(title='TEST', pk=1)
         self.assertEquals(Movie.objects.get(pk=1).title, 'TEST')
         self.assertEquals(Movie.objects.get(title='TEST').pk, 1)
+        self.assertNotEquals(Movie.objects.get(title='TEST').pk, 2)
+
+
+    def test_rating_creation(self):
+        test_rat = Rating.objects.create(stars=5, movie=43, rater=210, pk=1)
+        self.assertEquals(Rating.objects.get(pk=1).stars, 5)
+        self.assertEquals(Rating.objects.get(pk=1).movie, 43)
+        self.assertEquals(Rating.objects.get(pk=1).rater, 210)
+        self.assertEquals(Rating.objects.get(movie=43).pk, 1)
+        self.assertNotEquals(Rating.objects.get(movie=43).pk, 2)
+
+    def test_rater_creation(self):
+        test_rater = Rater.objects.create(pk=1, gender='M', zipcode='55555',
+                                          occupation=6, age=35)
+        self.assertEqual(Rater.objects.get(pk=1).gender, 'M')
+        self.assertEqual(Rater.objects.get(pk=1).zipcode, '55555')
+        self.assertEqual(Rater.objects.get(pk=1).occupation, 6)
+        self.assertEqual(Rater.objects.get(pk=1).age, 35)
+        self.assertGreater(User.objects.all().count(), 0)
