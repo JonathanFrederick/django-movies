862fbfb1faaaf10cb5c6bf2b7fe5fc160fdf4a93
Jonathan Frederick <jrfreder@gmail.com>
2015-10-11 17:13:42 -0400

    reading csv to dict

--- movieratings/ratingsdb/load_data.py
+++ movieratings/ratingsdb/load_data.py
@@ -1,6 +1,11 @@
+import csv
+
 def load_movies():
     with open('data/movies.dat', encoding='windows-1252') as f:
-        movies = [line.replace('::', '\t') for line in f]
-        print(movies)
+        reader = csv.DictReader([line.replace('::', '\t') for line in f],
+                                fieldnames=['movie_id', 'title', 'genres'],
+                                delimiter='\t')
+        for line in reader:
+            print(line)
 
 load_movies()
