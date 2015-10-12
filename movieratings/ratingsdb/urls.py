09a9752a976e24c4f4e08863d8112fa20a373236
Jonathan Frederick <jrfreder@gmail.com>
2015-10-11 22:34:05 -0400

    generating basic top 20 page

--- movieratings/ratingsdb/urls.py
+++ movieratings/ratingsdb/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^movies/top-20', views.top_20, name='top_20'),
+    url(r'^top-20', views.top_20, name='top_20'),
 
 ]
